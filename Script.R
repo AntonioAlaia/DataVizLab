128cf1fbf7c0d0df6d3eae357e81efd3b49b4e4e
Antonio Alaia <99255000+AntonioAlaia@users.noreply.github.com>
2022-04-01 19:52:46 +0200

    Scripts and BIGDATA EUROPE

--- Script.R
+++ Script.R
@@ -20,3 +20,28 @@ plot <- ggplot(data, aes(x=student_faculty_ratio, y=ranking_display)) +
 
 
 ### TRY TO DO THE SAME THING BUT WITH MORE UNIVER AND BEESWARM!!!
+
+new_df <- subset(df, c != 13) 
+
+bigdata <- import("qs-world-university-rankings-2017-to-2022-V2.csv")
+
+bigdata <- subset(bigdata, year != "2018") 
+bigdata <- subset(bigdata, year != "2017") 
+bigdata <- subset(bigdata, year != "2019") 
+bigdata <- subset(bigdata, year != "2020") 
+bigdata <- subset(bigdata, year != "2021") 
+
+view(bigdata)
+
+bigdata <- subset(bigdata, region != "Latin America") 
+bigdata <- subset(bigdata, region != "North America") 
+bigdata <- subset(bigdata, region != "Africa") 
+bigdata <- subset(bigdata, region != "Asia") 
+bigdata <- subset(bigdata, region != "Oceania") 
+
+ggplot(bigdata, aes(x=student_faculty_ratio, y=score)) +
+  geom_point() +
+  geom_smooth(method=lm , color="red", se=FALSE) +
+  theme_bw()
+
+export(bigdata,"europe.csv")
